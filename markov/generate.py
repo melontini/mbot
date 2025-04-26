01f51dca67e306d101089636e168665586af3a94
Autumn <journalistautumn@noreply.localhost>
2025-04-26 11:46:21 +0100

    fixed bsky posting code lol

--- markov/generate.py
+++ markov/generate.py
@@ -5,6 +5,7 @@ import sys
 import requests
 import json
 from datetime import datetime, timezone
+# from atproto import Client
 
 import secrets__
 
@@ -21,12 +22,24 @@ while not generated:
 text = text.replace('@','@​').replace('#','#​')
 print(text)
 
+# Fediverse posting code
 requests.post("https://cafe.autumn.town/api/iceshrimp/notes", json={
     'visibility': 'home',
     'text': text,
     #'cw': 'gyattov generated text', # Ruben has Left His Mark 
 }, headers={"Authorization": f"Bearer {secrets__.TOKEN}"})
 
+
+# Bsky posting code
+
+pds_url = "https://pds.lobotomy.town"
+session = requests.post(
+    pds_url + "/xrpc/com.atproto.server.createSession",
+    json={
+        "identifier": "markov.autumn.town",
+        "password": secrets__.BSKY,
+    },
+)
 # Fetch the current time
 # Using a trailing "Z" is preferred over the "+00:00" format
 now = datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
@@ -38,15 +51,13 @@ post = {
     "createdAt": now,
 }
 
-pds_url = "https://pds.lobotomy.town"
-
 resp = requests.post(
     pds_url + "/xrpc/com.atproto.repo.createRecord",
-    headers={"Authorization": f"Bearer {secrets__.BSKY}"},
+    headers={"Authorization": f"Bearer {session.json()['accessJwt']}"},
     json={
         "repo": "did:plc:vwcw6xd4udigfcylfhgh5deb",
         "collection": "app.bsky.feed.post",
         "record": post,
     },
 )
-# resp.raise_for_status()
+resp.raise_for_status()
