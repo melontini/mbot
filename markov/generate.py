b7fc5f8a792bdc2252cf3cdbc5b035f46192a909
Autumn <journalistautumn@noreply.localhost>
2025-05-06 11:03:22 +0100

    forgot to uncomment the code that posts it oops

--- markov/generate.py
+++ markov/generate.py
@@ -22,42 +22,42 @@ while not generated:
 text = text.replace('@','@​').replace('#','#​')
 print(text)
 
-# # Fediverse posting code
-# requests.post("https://cafe.autumn.town/api/iceshrimp/notes", json={
-#     'visibility': 'home',
-#     'text': text,
-#     #'cw': 'gyattov generated text', # Ruben has Left His Mark 
-# }, headers={"Authorization": f"Bearer {secrets__.TOKEN}"})
+# Fediverse posting code
+requests.post("https://cafe.autumn.town/api/iceshrimp/notes", json={
+    'visibility': 'home',
+    'text': text,
+    #'cw': 'gyattov generated text', # Ruben has Left His Mark 
+}, headers={"Authorization": f"Bearer {secrets__.TOKEN}"})
 
 
-# # Bsky posting code
+# Bsky posting code
 
-# pds_url = "https://pds.lobotomy.town"
-# session = requests.post(
-#     pds_url + "/xrpc/com.atproto.server.createSession",
-#     json={
-#         "identifier": "markov.autumn.town",
-#         "password": secrets__.BSKY,
-#     },
-# )
-# # Fetch the current time
-# # Using a trailing "Z" is preferred over the "+00:00" format
-# now = datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
+pds_url = "https://pds.lobotomy.town"
+session = requests.post(
+    pds_url + "/xrpc/com.atproto.server.createSession",
+    json={
+        "identifier": "markov.autumn.town",
+        "password": secrets__.BSKY,
+    },
+)
+# Fetch the current time
+# Using a trailing "Z" is preferred over the "+00:00" format
+now = datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
 
-# # Required fields that each post must include
-# post = {
-#     "$type": "app.bsky.feed.post",
-#     "text": text,
-#     "createdAt": now,
-# }
+# Required fields that each post must include
+post = {
+    "$type": "app.bsky.feed.post",
+    "text": text,
+    "createdAt": now,
+}
 
-# resp = requests.post(
-#     pds_url + "/xrpc/com.atproto.repo.createRecord",
-#     headers={"Authorization": f"Bearer {session.json()['accessJwt']}"},
-#     json={
-#         "repo": "did:plc:vwcw6xd4udigfcylfhgh5deb",
-#         "collection": "app.bsky.feed.post",
-#         "record": post,
-#     },
-# )
-# resp.raise_for_status()
+resp = requests.post(
+    pds_url + "/xrpc/com.atproto.repo.createRecord",
+    headers={"Authorization": f"Bearer {session.json()['accessJwt']}"},
+    json={
+        "repo": "did:plc:vwcw6xd4udigfcylfhgh5deb",
+        "collection": "app.bsky.feed.post",
+        "record": post,
+    },
+)
+resp.raise_for_status()
